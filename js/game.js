3be040b62166cbdf4eeef6b6b737eaf51d25afad
Mark Katerberg <katerberg@fastmail.fm>
2022-02-03 18:45:11 -0600

    Bigger canvas

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 function setupCanvas() {
   const canvas = document.querySelector("canvas");
   ctx = canvas.getContext("2d");
+  canvas.width = game.tileSize * (game.numTiles + game.uiWidth);
+  canvas.height = game.tileSize * game.numTiles;
 }
 
